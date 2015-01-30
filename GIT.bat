677d2406fa0de13875cc8d09b226cc701f92ba7f
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2015-01-30 16:14:53 +0100

    Create GIT.bat

---
+++ GIT.bat
@@ -0,0 +1,13 @@
+@echo off
+cls
+title Command Line (CL) - Git
+
+echo Command Line (CL) - Git Program
+echo (C) Copyright 2014-present Suriyaa Kudo
+echo.
+
+ver
+:cmd
+cd git
+cd bin
+sh.exe --login -i
